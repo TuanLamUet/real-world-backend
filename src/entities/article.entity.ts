ceb77802c536ed13a32241f52a0673d588e8f039
Lâm Tuấn <tupikano1@gmail.com>
2020-04-28 22:16:59 +0700

    config article entities

---
+++ src/entities/article.entity.ts
@@ -0,0 +1,59 @@
+import { Entity, Column, ManyToMany, JoinColumn, RelationCount, ManyToOne, BeforeInsert } from 'typeorm';
+import { classToPlain } from 'class-transformer';
+import * as slugify from 'slug';
+import { AbstractEntity } from './abstract-entity';
+import { UserEntity } from './user.entity';
+
+@Entity('articles')
+export class ArticleEntity extends AbstractEntity {
+  @Column()
+  slug: string;
+
+  @Column()
+  title: string;
+
+  @Column()
+  description: string;
+
+  @Column()
+  body: string;
+
+  @ManyToMany(
+    type => UserEntity,
+    user => user.favorites,
+    { eager: true},
+  )
+  @JoinColumn()
+  favoritedBy: UserEntity[];
+
+  @RelationCount((article: ArticleEntity) => article.favoritedBy)
+  favoritesCount: number;
+  
+  @ManyToOne(
+    type => UserEntity,
+    user => user.articles,
+    { eager: true}
+  )
+  author: UserEntity;
+  
+  @Column('simple-array')
+  tagList: string[];
+
+  @BeforeInsert()
+  generateSlug() {this.slug = slugify(this.title, { lower: true})+ '-' + ((Math.random() * Math.pow(36, 6)) | 0).toString(36);
+  }
+
+  toJSON() {
+    return classToPlain(this);
+  }
+
+  toArticle(user?: UserEntity) {
+    let favorited = null;
+    if(user) {
+      favorited = this.favoritedBy.includes(user);
+    }
+    const article: any = this.toJSON();
+    delete article.favoritedBy;
+    return { ...article, favorited}
+  }
+}
--- src/entities/user.entity.ts
+++ src/entities/user.entity.ts
@@ -1,8 +1,10 @@
-import {AbstractEntity} from './abstract-entity';
-import { Entity, Column, BeforeInsert } from 'typeorm';
+import { Entity, Column, BeforeInsert, ManyToMany, JoinTable, OneToMany, JoinColumn } from 'typeorm';
 import * as bcrypt from 'bcryptjs';
 import { IsEmail } from 'class-validator';
 import { Exclude, classToPlain } from 'class-transformer';
+import {AbstractEntity} from './abstract-entity';
+import { ArticleEntity } from './article.entity';
+import { type } from 'os';
 
 @Entity('user')
 export class UserEntity extends AbstractEntity{
@@ -23,6 +25,32 @@ export class UserEntity extends AbstractEntity{
   @Exclude()
   password: string;
 
+  @ManyToMany(
+    type => UserEntity,
+    user => user.followee,
+  )
+  @JoinTable()
+  followers: UserEntity[];
+
+  @ManyToMany( 
+    type => UserEntity,
+    user => user.followers
+  )
+  followee: UserEntity[];
+
+  @OneToMany(
+    type => ArticleEntity,
+    article => article.author,
+  )
+  articles: ArticleEntity[];
+
+  @ManyToMany(
+    type => ArticleEntity,
+    article => article.favoritedBy,
+  )
+  @JoinColumn()
+  favorites: ArticleEntity[];
+
 
   @BeforeInsert()
   async hashPassword() {
@@ -35,5 +63,15 @@ export class UserEntity extends AbstractEntity{
 
   toJSON() {
     return classToPlain(this);
+  }
+
+  toProfile(user?: UserEntity) {
+    let following = null;
+    if(user) {
+      following = this.followers.includes(user);
+    }
+    const profile: any = this.toJSON();
+    delete profile.followers;
+    return { ...profile, following}
   }
 }
