66938508e56b9518143a73faffab103a768719e2
Lâm Tuấn <tupikano1@gmail.com>
2020-04-28 23:04:12 +0700

    finish feature article

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,11 +5,15 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { DatabaseConnectionService } from './database-connection.service';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
+import { ArticleController } from './article/article.controller';
+import { ArticleService } from './article/article.service';
+import { ModuleService } from './module/module.service';
+import { ArticleModule } from './article/article.module';
 @Module({
   imports: [TypeOrmModule.forRootAsync({
     useClass: DatabaseConnectionService
-  }), AuthModule, UserModule],
-  controllers: [AppController],
-  providers: [AppService],
+  }), AuthModule, UserModule, ArticleModule],
+  controllers: [AppController, ArticleController],
+  providers: [AppService, ArticleService, ModuleService],
 })
 export class AppModule {}
